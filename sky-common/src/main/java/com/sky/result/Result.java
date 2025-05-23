e061598e3d1b41c1b746124dcf9f5fb5a60d3088
liquor <rose@163.com>
2025-05-23 20:08:32 +0800

    sky-take-out

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  * @param <T>
  */
 @Data
-public class Result<T> implements Serializable {
+public class   Result<T> implements Serializable {
 
     private Integer code; //编码：1成功，0和其它数字为失败
     private String msg; //错误信息
